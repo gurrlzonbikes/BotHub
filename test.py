d8b28fb7dcfa7a173d3b2e261db9295cb4b33bce
gurrlzonbikes <zombiecuicui@mailinator.com>
2014-09-25 21:16:57 +0200

    trying to fix openssl

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import requests, json, ssl
+from SSLAdapter import SSLAdapter
 
-
-cacert = '/Library/Python/2.7/site-packages/requests/cacert.pem'
-r = requests.get('https://api.github.com/gurrlzonbikes', auth=('gurrlzonbikes', 'starcraft666'), verify='/Library/Python/2.7/site-packages/requests/cacert.pem')
+auth=('gurrlzonbikes', 'starcraft666')
+s = requests.Session()
+s.mount('https://', SSLAdapter())
+"""r = requests.get('https://api.github.com/gurrlzonbikes', auth=('gurrlzonbikes', 'starcraft666'), verify='/Library/Python/2.7/site-packages/requests/cacert.pem')
 print(r.status_code)
+"""
+s.get('https://api.github.com/gurrlzonbikes')
+print(s.status_code)
