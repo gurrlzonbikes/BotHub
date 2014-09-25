6fef4226ca24a1c4de3c734aa4a7f9912a26480d
gurrlzonbikes <zombiecuicui@mailinator.com>
2014-09-25 21:18:26 +0200

    test

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ from SSLAdapter import SSLAdapter
 auth=('gurrlzonbikes', 'starcraft666')
 s = requests.Session()
 s.mount('https://', SSLAdapter())
+
 """r = requests.get('https://api.github.com/gurrlzonbikes', auth=('gurrlzonbikes', 'starcraft666'), verify='/Library/Python/2.7/site-packages/requests/cacert.pem')
-print(r.status_code)
-"""
+print(r.status_code)liuhilfilhudfliuahfuh"""
+
 s.get('https://api.github.com/gurrlzonbikes')
 print(s.status_code)
