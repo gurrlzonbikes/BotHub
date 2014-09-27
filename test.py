56bbf5b90f65bc94fd622e0c205110c09de78278
gurrlzonbikes <zombiecuicui@mailinator.com>
2014-09-27 11:26:19 +0200

    upgraded to python 3 fixed requests issues

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests, json, ssl
 from SSLAdapter import SSLAdapter
 
+print(SSLAdapter)
 auth=('gurrlzonbikes', 'starcraft666')
 s = requests.Session()
 s.mount('https://', SSLAdapter())
@@ -8,5 +9,7 @@ s.mount('https://', SSLAdapter())
 """r = requests.get('https://api.github.com/gurrlzonbikes', auth=('gurrlzonbikes', 'starcraft666'), verify='/Library/Python/2.7/site-packages/requests/cacert.pem')
 print(r.status_code)liuhilfilhudfliuahfuh"""
 
-s.get('https://api.github.com/gurrlzonbikes')
-print(s.status_code)
+s.get('https://api.github.com')
+print(s.text)
+
+
