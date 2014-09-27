1a99272a9d6ae2a00971d8fae891b2b33ee48428
gurrlzonbikes <zombiecuicui@mailinator.com>
2014-09-27 13:06:37 +0200

    so json, much response

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
 import requests, json, ssl
 from SSLAdapter import SSLAdapter
 
-print(SSLAdapter)
-auth=('gurrlzonbikes', 'starcraft666')
+"""print(SSLAdapter)
 s = requests.Session()
-s.mount('https://', SSLAdapter())
+s.mount('https://', SSLAdapter())"""
 
-"""r = requests.get('https://api.github.com/gurrlzonbikes', auth=('gurrlzonbikes', 'starcraft666'), verify='/Library/Python/2.7/site-packages/requests/cacert.pem')
-print(r.status_code)liuhilfilhudfliuahfuh"""
+auth=('gurrlzonbikes', 'starcraft666')
 
-s.get('https://api.github.com')
-print(s.text)
+r = requests.get('https://api.github.com/repos/gurrlzonbikes/BotHub')
+bothub_repo = r.json()
+#print(json.dumps(bothub_repo, sort_keys=True))
 
+list_rep = requests.get('https://api.github.com/users/gurrlzonbikes/repos')
+repo_for_her = list_rep.json()
+#print(json.dumps(repo_for_her, sort_keys=True))
+
+withauth = requests.get('https://api.github.com/user', auth=('gurrlzonbikes', 'starcraft666'))
+success = withauth.json()
+#print(json.dumps(success, sort_keys=True))
+
+input = {"name" : "Fordwidwi", "auto_init":True}
+create_repo = requests.post('https://api.github.com/user/repos', auth=('gurrlzonbikes', 'starcraft666'), data=input)
+print(create_repo.text)
+#print(json.dumps(repo_for_her, sort_keys=True))
 
