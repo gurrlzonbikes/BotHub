37aad861cb7108a5945a6d3bd32767833992d175
gurrlzonbikes <zombiecuicui@mailinator.com>
2014-09-28 10:35:40 +0200

    rm useless

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-import requests, json, ssl
-from SSLAdapter import SSLAdapter
+import requests, json
+import tkinter
 
-"""print(SSLAdapter)
-s = requests.Session()
-s.mount('https://', SSLAdapter())"""
-
-auth=('gurrlzonbikes', 'starcraft666')
+user = input("Die name??!")
+pwd = input("Password?")
+auth=(user, pwd)
 
 r = requests.get('https://api.github.com/repos/gurrlzonbikes/BotHub')
 bothub_repo = r.json()
@@ -15,12 +13,16 @@ list_rep = requests.get('https://api.github.com/users/gurrlzonbikes/repos')
 repo_for_her = list_rep.json()
 #print(json.dumps(repo_for_her, sort_keys=True))
 
-withauth = requests.get('https://api.github.com/user', auth=('gurrlzonbikes', 'starcraft666'))
+withauth = requests.get('https://api.github.com/user', auth=auth)
 success = withauth.json()
 #print(json.dumps(success, sort_keys=True))
 
-input = {"name" : "Fordwidwi", "auto_init":True}
-create_repo = requests.post('https://api.github.com/user/repos', auth=('gurrlzonbikes', 'starcraft666'), data=input)
-print(create_repo.text)
+headers = {'content-type': 'application/json'}
+params = {"name" : "Fordwidwi", "auto_init":True}
+#create_repo = requests.post('https://api.github.com/user/repos', auth=('gurrlzonbikes', 'starcraft666'), data=json.dumps(params))
+#print(json.dumps(create_repo.json()))
 #print(json.dumps(repo_for_her, sort_keys=True))
 
+get_hot_repos = requests.get("https://api.github.com/search/repositories")
+resp = get_hot_repos.json()
+print(json.dumps(resp, sort_keys=True))
