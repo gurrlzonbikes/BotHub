6c9277119f8bc8c2c4ff7d4ab0ec58808030b37c
gurrlzonbikes <zombiecuicui@mailinator.com>
2014-11-18 09:41:57 +0100

    got it to clone somewhere else w/ subprocess

--- repoManagement.py
+++ repoManagement.py
@@ -4,5 +4,7 @@ import subprocess
 class repoManagement:
 
     def clone_repo(self, repo_url):
-        subprocess.call('cd ../', shell=True)
-        subprocess.call(['git clone '+repo_url], shell=True)
+        #subprocess.call('cd ../', shell=True)
+        #subprocess.call(['git clone '+repo_url], shell=True)
+        p = subprocess.Popen(['git clone '+repo_url], cwd='/Users/Tual/PycharmProjects', shell=True)
+        p.wait()
